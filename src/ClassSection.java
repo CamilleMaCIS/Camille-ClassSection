792a9efbfc89ffdfaa7c67575015fc3f1b20b820
CamilleMaCIS <mehawesomegirl@gmail.com>
2023-02-24 15:43:26 +0800

    ClassSectionTester had import error, fixed it. Edited ClassSection and Student

--- src/ClassSection.java
+++ src/ClassSection.java
@@ -1,2 +1,72 @@
+import java.util.ArrayList;
+
 public class ClassSection {
+    //properties
+    private String subject;
+    private int capacity;
+    private int yearLevel;
+    private ArrayList<Student> students;
+
+    //constructor
+    public ClassSection(String subject, int capacity, int yearLevel){
+        this.subject = subject;
+        this.capacity = capacity;
+        this.yearLevel = yearLevel;
+        this.students = new ArrayList<>();
+    }
+
+    //methods
+    public String getSubject() {
+        return subject;
+    }
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+    public int getCapacity() {
+        return capacity;
+    }
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+    public int getYearLevel() {
+        return yearLevel;
+    }
+    public void setYearLevel(int yearLevel) {
+        this.yearLevel = yearLevel;
+    }
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    private boolean sameYearLevel(Student addStudent){
+        if (addStudent.getYearLevel() != this.yearLevel){
+            return false;
+        }
+        return true;
+    }
+    public void addStudent(Student student){
+        int count = 0;
+        for (Student existStudent : students){
+            if (existStudent.getId() != student.getId()){
+                count++;
+            }
+        }
+        //will not add conflicting ID, or students not in this year level
+        if (count == students.size() && sameYearLevel(student)){
+            this.students.add(student);
+        }
+    }
+    public void removeStudent(Student student){
+        this.students.remove(student);
+    }
+    public boolean isStudentEnrolled(Student student){
+        if (students.contains(student)){
+            return true;
+        }
+        return false;
+    }
+
+    public String toString(){
+        return "ClassSection{subject='" + this.subject + "', capacity=" + this.capacity + ", students=" + this.students + "}";
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,69 @@
 public class Student {
+    //properties
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private int yearLevel;
+    private static int studentCount = 0;
+    public static int createID()
+    {
+        return studentCount++;
+    }
+
+    //constructor
+    public Student(String firstName, String lastName, int age, int yearLevel){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.yearLevel = yearLevel;
+        this.id = createID();
+    }
+
+    //methods
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getYearLevel() {
+        return yearLevel;
+    }
+    public void setYearLevel(int yearLevel) {
+        this.yearLevel = yearLevel;
+    }
+    public static int getStudentCount() {
+        return studentCount;
+    }
+
+    public boolean equals(Student other){
+        if (other.firstName.equals(this.firstName) && other.lastName.equals(this.lastName)){
+            if (other.age == this.age && other.yearLevel == this.yearLevel){
+                return true;
+            }
+        }
+        return false;
+    }
+    public String toString(){
+        return "Student{id=" + this.id + ", firstName='" + this.firstName + "', lastName='" + this.lastName + "', age=" + this.age + "}";
+    }
 }
